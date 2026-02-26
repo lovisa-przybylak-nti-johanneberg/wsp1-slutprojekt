cfcc9417a68aab3e9601ff7be9dfa190079b5c7c
Lovisa <lovisa.przybylak@gmail.com>
2026-02-26 16:58:37 +0100

    Den fungerar inte?

--- db/seeder.rb
+++ db/seeder.rb
@@ -6,4 +6,22 @@ class Seeder
     p "doit"
   end
 
+  def self.drop_tables
+    db.execute('DROP TABLE IF EXISTS places')
+  end
+
+  def self.create_tables
+    db.execute('CREATE TABLE places (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                rating INTEGER, 
+                review TEXT)')
+  end
+
+  def self.populate_tables
+    db.execute('INSERT INTO places (name, rating, review) VALUES ("Skansen Kronan", 7, "Fin utsikt över Haga, Vasa och Linné!")')
+    db.execute('INSERT INTO places (name, rating, review) VALUES ("Änggården", 5, "En fin promenad, särskilt på sommaren.")')
+    db.execute('INSERT INTO places (name, rating, review) VALUES ("Karlatornet", 3, "Fin utsikt men man kommer inte in utan att känna någon som bor där")')
+  end
+
 end
